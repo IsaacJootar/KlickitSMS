c0dcc61126cc81ccc5a787663f62509b155c6b73
IZICC\ISAAC <jootarisaac@gmail.com>
2021-05-21 08:20:42 +0100

    clean up commenting

--- classes/accountmonthlypayrollconfig.php
+++ classes/accountmonthlypayrollconfig.php
@@ -1,6 +1,4 @@
 <?php
-// If it's going to need the database, then it's 
-// probably smart to require it before we start.
 require_once('database.php');
 
 class accountsmonthlypayrollconfig {
--- classes/result_manager_cumulative.php
+++ classes/result_manager_cumulative.php
@@ -1,7 +1,5 @@
 <?php
 ob_start();
-// If it's going to need the database, then it's 
-// probably smart to require it before we start.
 require_once('database.php');
 include('includes/config2.php');
 
